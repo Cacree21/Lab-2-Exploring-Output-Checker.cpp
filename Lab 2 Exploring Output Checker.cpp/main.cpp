ad8d90cc4162552e0fd05d97f4295d8b9b3d6b1a
Cacree21 <111894319+Cacree21@users.noreply.github.com>
2022-09-19 09:23:09 -0500

    Update main.cpp
    Lab 3 Exploring Outputs

--- Lab 2 Exploring Output Checker.cpp/main.cpp
+++ Lab 2 Exploring Output Checker.cpp/main.cpp
@@ -1,14 +1,29 @@
-//
-//  main.cpp
-//  Lab 2 Exploring Output Checker.cpp
-//
-//  Created by Caleb Acree on 9/13/22.
-//
+/*
+Caleb Acree
+C++ Fall 2022
+Due: September 14, 2022
+Lab 2 Exploring Output
+This lab is designed to get you familiar output to the command prompt using C++
+ */
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    const string s = "   *";
+    const string S = "        *";
+    const string st = "*";
+    
+    cout << s << S << S << S;
+    cout << endl << endl << st << S << S << S;
+    cout << endl << endl << s << S << S << S;
+    cout << endl << endl << st << S << S << S;
+    cout << endl << endl << s << S << S << S;
+    cout << endl << endl << st << S << S << S;
+    cout << endl << endl << s << S << S << S;
+    cout << endl << endl << st << S << S << S;
+    
     return 0;
 }
